cfb443c1e6c3e38c2dff6eaf501f5819da6263ff
Ervin Castro Perez <ervindevc@gmail.com>
2024-04-09 18:50:53 -0500

    update callbakcs

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,22 +1,30 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-//llamado de la api, se coloca API en mayusculas para hacer referencia que es in valor que no va a cambair dentro de los archivos.
+//llamado de la api, se coloca API en mayusculas para hacer referencia que es valor que no va a cambair dentro de los archivos, referencia del root
 const API = "https://api.escuelajs.co/api/v1";
 
-//funcion que recive la url y el callback, para recivir la solicitud de la api
+//funcion que recive la url y el callback que es la informacion que nos va a retornar los elementos ya sea la data o el error que nos pueda generar
 function fecthData(urlApi, callback) {
-  //url de app, el callback recibe la informacion de los elementos, o el error de
+
+  //llamado a xhttp
   let xhttp = new XMLHttpRequest(); //realiza la solicitud
 
-  //trabajar con xhttp,el open es para abrir la conexion,(obtener, la url que utilizara, habilitar la api)
+  //trabajar con xhttp,el open es para abrir la conexion,(obtener, la url que utilizara, habilitar la api);
+  //GET: El tipo de peticion que se va a realizar
+  //urlApi: es la url que va a utilizar.
+  //true: el valor que va a habilitar
   xhttp.open("GET", urlApi, true);
-  //onreadystatechange es para escuchar el estado de la solicitud y saber cuando esta disponible la informacion
+
+
+  //onreadystatechange es parte de los reccursos que vienen en xhttp, es para escuchar los diferentes estados que tiene la solicitud y saber cuando esta disponible la informacion la información 
   xhttp.onreadystatechange = function (event) {
     //event es para detectar cambios en el codigo.
+
     //validar estado en el que se encuentra
     if (xhttp.readyState === 4) {
       //validar el mismo tipo de valor y dato
       if (xhttp.status === 200) {
+
         callback(null, JSON.parse(xhttp.responseText)); // dentro de xhttp.responseTextx recive las peticiones del sevidor
       } else {
         const error = new Error("Error" + urlApi); //saber en que elemento en particual fue el error de la api
@@ -29,9 +37,11 @@ function fecthData(urlApi, callback) {
 
 fecthData(`${API}/products`, function (error1, data1) {
   //api y peticion, funcion anonima
+
   //validar que si hay un error detenga toda ejecucion
   if (error1) return console.error(error1); //devuelve el error que encuentre
   //si la peticion es correcta llamara al producto
+  
   //paso de parametros y funcion que es callback, en el data1 recibe el valor
   fecthData(`${API}/products/${data1[0].id}`, function (error2, data2) {
     //llamado de los elementos
